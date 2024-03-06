f2144d2eb831242eeb69cd418b855b15bcaaac09
Nikola Kenjic <nikola.nico89@gmail.com>
2024-03-06 20:52:18 +0100

    backend added

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import { StatusCodes } from 'http-status-codes';
 import { connectDB } from './mongoDB/connect.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
+app.use(express.static(join(__dirname, 'client/dist')));
 app.use(express.json());
 app.use(cookieParser());
 
+// Import Routes
 import authRouter from './routes/authRoutes.js';
 import cryptoRouter from './routes/cryptoRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import { authenticateUser } from './middleware/authenticateUser.js';
 
-// Test routes
-app.get('/api/v1/test', (req, res, next) => {
-  res.json({ msg: 'testing route' });
-});
-
 // AuthRouter
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/currency', cryptoRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+});
 
 // Errors
 app.use('*', (req, res, next) => {
