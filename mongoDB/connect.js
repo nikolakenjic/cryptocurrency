f4fea171a667f9e73e91eebacda8f1b97bc1e9a5
Nikola Kenjic <nikola.nico89@gmail.com>
2024-10-09 18:47:11 +0200

    connect

--- mongoDB/connect.js
+++ mongoDB/connect.js
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose';
 
-export const connectDB = (url) => {
+export const connectDB = async (url) => {
   console.log('Connecting to DB...');
-  return mongoose.connect(url);
+  try {
+    const connect = await mongoose.connect(url);
+    console.log(`MongoDB connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
 };
